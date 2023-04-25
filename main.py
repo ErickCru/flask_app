b620689dea683599d097ab99a13088eaa9da05c5
Erick Cruz <ecruz@phidimensions.com>
2023-04-25 09:46:39 -0500

    Update endpoints

--- main.py
+++ main.py
@@ -9,9 +9,21 @@ home_message = """<h1>Demo app</h1>
 This app consumes the <a href="https://jsonplaceholder.typicode.com">{JSON} Placeholder</a> service.<br/><br/>
 <h3>Endpoints available:</h3>
 <ul>
-<li>/users</li>
-<li>/users/{id}</li>
+<li>/users/{id}/albums</li>
+<li>/users/{id}/posts</li>
 </ul>"""
+
+
+def validate_data(data, id):
+    if len(data.json()) == 0:
+        content_type = data.headers["content-type"]
+        status_code = 404
+        data = json.dumps({"message": f"User {id} not found"})
+    else:
+        content_type = data.headers["content-type"]
+        status_code = data.status_code
+        data = data.text
+    return data, status_code, content_type
 
 
 @app.route("/")
@@ -19,25 +31,23 @@ def home():
     return home_message
 
 
-@app.route("/users")
-def users():
-    data = requests.get(base_url)
-
+@app.route("/users/<id>/albums")
+def users(id):
+    data = requests.get(f"{base_url}/{id}/albums")
+    data, status_code, content_type = validate_data(data, id)
     return Response(
-        data.text,
-        status=data.status_code,
-        content_type=data.headers["content-type"],
+        data,
+        status=status_code,
+        content_type=content_type,
     )
 
 
-@app.route("/users/<id>")
+@app.route("/users/<id>/posts")
 def user(id):
-    data = requests.get(f"{base_url}/{id}")
-
+    data = requests.get(f"{base_url}/{id}/posts")
+    data, status_code, content_type = validate_data(data, id)
     return Response(
-        json.dumps({"message": f"User {id} not found"})
-        if data.status_code == 404
-        else data.text,
-        status=data.status_code,
-        content_type=data.headers["content-type"],
+        data,
+        status=status_code,
+        content_type=content_type,
     )
